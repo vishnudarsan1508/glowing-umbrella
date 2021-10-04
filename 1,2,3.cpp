0ee7816fcd9155953844d540725217fd97ef3f5a
vishnudarsan1508 <73700960+vishnudarsan1508@users.noreply.github.com>
2021-10-04 19:53:15 +0530

    Add files via upload

---
+++ 1,2,3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+		int i,j,max;
+		cout<<"Enter max number : ";
+		cin>>max;
+		for(i=1;j<=max;j++)
+		{
+			for(i=1;i<=j;i++)
+			{
+				cout<<i<<" ";
+			}
+				cout<<"\n";
+		}			
+}		
